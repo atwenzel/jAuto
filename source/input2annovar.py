3043574267bbf3730082156f4cfe6259fddf9501
atwenzel <alexanderwenzel2017@u.northwestern.edu>
2015-11-25 21:38:11 -0600

    first

---
+++ source/input2annovar.py
@@ -0,0 +1,14 @@
+"""These scripts take the input files, given as csv, and creates an avinput file for
+Annovar annotation.  
+
+Each line in config should have
+filename,sample-col-num,chr-col-num,poshg19-col-num,basechange-col
+
+TODO: Support for .xlsx"""
+
+#Global
+
+#Local
+
+if __name__ == "__main__":
+    print("This script converts .csv files to .avinput for Annovar.")
---
+++ source/jauto.py
@@ -0,0 +1,15 @@
+"""
+This is the driver file for jAuto.  For a full documentation, please see README.  
+
+Original analysis/annotation pipeline developed by Joonhee Park.  
+Python implementation by Alex Wenzel (alexanderwenzel2017@u.northwestern.edu)
+"""
+
+#Global
+import openpyxl
+import sys
+
+#Local
+
+if __name__ == "__main__":
+    print("This is the jAuto entry point")
